fc5488ccbed15ad82858317eed8d4933a14ca81b
sujinsin <sinsu0051@naver.com>
2023-03-10 17:16:55 +0900

    회원 프로필 메서드 성공할 경우 in UserServiceTest

--- src/test/java/com/cos/photogramstart/service/UserServiceTest.java
+++ src/test/java/com/cos/photogramstart/service/UserServiceTest.java
@@ -1,16 +1,18 @@
 package com.cos.photogramstart.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.anyInt;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -22,10 +24,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.likes.Likes;
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import com.cos.photogramstart.handler.ex.CustomException;
+import com.cos.photogramstart.web.dto.user.UserProfileDto;
 
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
@@ -52,15 +56,6 @@ public class UserServiceTest {
 		
 		@Mock
 		private User userEntity= User.builder().id(pageUserId).build();
-		private final Image image1 = Image.builder().caption("이미지1").likeCount(3)
-				.postImageUrl("https://localhost/images/image1.png").build();
-		private final Image image2 = Image.builder().caption("이미지2").likeCount(5)
-				.postImageUrl("https://localhost/images/image2.png").build();
-
-		@BeforeEach
-		void setUp() {
-			userEntity.setImages(Stream.of(image1, image2).collect(Collectors.toList()));
-		}
 
 		@Test
 		@DisplayName("페이지 사용자가 존재하지 않을 경우 예외를 던진다.")
@@ -79,6 +74,41 @@ public class UserServiceTest {
 			// naver() 메서드가 호출이 되지 않았는지 여부 검즘
 			// anyInt() 매개변수 값이 어떤 값이든 상관없다.
 
+		}
+		
+		@Test
+		@DisplayName("페이지 사용자가 존재할 경우 리스트를 리턴해준다.")
+		void return_list_when_pageUserId_exist() {
+			
+			int pageUserId = 2;
+			int principalId = 1;
+			
+			List<Image> images = new ArrayList<>();
+			Image image1 = new Image();
+			image1.setLikes(Arrays.asList(new Likes(), new Likes()));
+			images.add(image1);
+			Image image2 = new Image();
+			image2.setLikes(Arrays.asList(new Likes()));
+			images.add(image2);
+			
+			when(userEntity.getImages()).thenReturn(images);
+			when(userRepository.findById(pageUserId)).thenReturn(Optional.of(userEntity));
+			when(subscribeRepository.mSubscribeState(principalId, pageUserId)).thenReturn(1);
+			when(subscribeRepository.mSubscribeCount(pageUserId)).thenReturn(3);
+			
+			UserProfileDto userProfileDto = new UserProfileDto();
+			userProfileDto.setUser(userEntity);
+			userProfileDto.setImageCount(userEntity.getImages().size());
+			userProfileDto.setSubscribeCount(3);
+			userProfileDto.setSubscribeState(subscribeRepository.mSubscribeState(principalId, pageUserId) == 1 );
+			
+		    UserProfileDto result = userService.회원프로필(pageUserId, principalId);
+		    
+		    assertNotNull(result);
+		    assertEquals(result.isSubscribeState(), true);
+		    assertEquals(result.getSubscribeCount(), 3);
+		    assertEquals(result.getUser(),userEntity);
+		    assertEquals(result,userProfileDto);
 		}
 	}
 }
